16667f537b0dc692587bae5bdcf52d735d3e5e2f
Brandon Liu <bliu88@live.com>
2019-01-31 16:02:12 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,4 @@
 # Write your #display_rainbow method here
-def display_rainbow(color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
-  puts "R:{color[1]}"
+def display_rainbow(colors)
+  puts "#{colors[0]}, #{colors[1]}, #{colors[2]}, #{colors[3]}, #{colors[4]}, #{colors[5]}, #{colors[6]}"
+end
