92af846b3d0c60ef6a46f4f13189af13680cb128
Brandon Liu <bliu88@live.com>
2019-01-31 15:59:32 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,2 +1,2 @@
 # Write your #display_rainbow method here
-  puts "R:{color[1]}"def display_rainbow(color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+ef display_rainbow(color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
